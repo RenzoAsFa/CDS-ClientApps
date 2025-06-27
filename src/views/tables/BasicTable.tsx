13cd576cd60ade6799a284e5af6eeb290d688484
RenzoAsFa <rascona@corsusa.com>
2025-06-27 16:21:41 -0500

    feat: add access table & conn

--- src/views/tables/BasicTable.tsx
+++ src/views/tables/BasicTable.tsx
@@ -1,153 +1,191 @@
-import {
-  Typography,
-  Box,
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableRow,
-  Chip,
-  TableContainer,
-} from "@mui/material";
-
-import BaseCard from "src/components/BaseCard/BaseCard";
+import React from 'react';
+// import '.src/App.css';
+import 'src/App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import axios from 'axios';
+import { Modal, ModalBody, ModalFooter, ModalHeader, Table } from 'reactstrap';
+import { useState, useEffect } from 'react';
 
 
-const products = [
-  {
-    id: "1",
-    name: "Sunil Joshi",
-    post: "Web Designer",
-    pname: "Elite Admin",
-    priority: "Low",
-    pbg: "primary.main",
-    budget: "3.9",
-  },
-  {
-    id: "2",
-    name: "Andrew McDownland",
-    post: "Project Manager",
-    pname: "Real Homes WP Theme",
-    priority: "Medium",
-    pbg: "secondary.main",
-    budget: "24.5",
-  },
-  {
-    id: "3",
-    name: "Christopher Jamil",
-    post: "Project Manager",
-    pname: "MedicalPro WP Theme",
-    priority: "High",
-    pbg: "error.main",
-    budget: "12.8",
-  },
-  {
-    id: "4",
-    name: "Nirav Joshi",
-    post: "Frontend Engineer",
-    pname: "Hosting Press HTML",
-    priority: "Critical",
-    pbg: "success.main",
-    budget: "2.4",
-  },
-];
-
-const BasicTable = () => {
-  return (
-    <BaseCard title="Basic Table">
-
-      <TableContainer
-        sx={{
-          width: {
-            xs: "274px",
-            sm: "100%",
-          },
-        }}
-      >
-        <Table
-          aria-label="simple table"
-          sx={{
-            whiteSpace: "nowrap",
-          }}
-        >
-          <TableHead>
-            <TableRow>
-              <TableCell>
-                <Typography variant="subtitle1">
-                  Id
-                </Typography>
-              </TableCell>
-              <TableCell>
-                <Typography variant="subtitle1">
-                  Assigned
-                </Typography>
-              </TableCell>
-              <TableCell>
-                <Typography variant="subtitle1">
-                  Name
-                </Typography>
-              </TableCell>
-              <TableCell>
-                <Typography variant="subtitle1">
-                  Priority
-                </Typography>
-              </TableCell>
-              <TableCell align="right">
-                <Typography variant="subtitle1">
-                  Budget
-                </Typography>
-              </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {products.map((product) => (
-              <TableRow key={product.name}>
-                <TableCell>
-                  <Typography fontSize="15px" fontWeight={500}>
-                    {product.id}
-                  </Typography>
-                </TableCell>
-                <TableCell>
-                  <Box display="flex" alignItems="center">
-                    <Box>
-                      <Typography fontSize="14px" fontWeight={600}>
-                        {product.name}
-                      </Typography>
-                      <Typography color="textSecondary" fontSize="13px">
-                        {product.post}
-                      </Typography>
-                    </Box>
-                  </Box>
-                </TableCell>
-                <TableCell>
-                  <Typography color="textSecondary" fontSize="14px">
-                    {product.pname}
-                  </Typography>
-                </TableCell>
-                <TableCell>
-                  <Chip
-                    sx={{
-                      pl: "4px",
-                      pr: "4px",
-                      backgroundColor: product.pbg,
-                      color: "#fff",
-                    }}
-                    size="small"
-                    label={product.priority}
-                  ></Chip>
-                </TableCell>
-                <TableCell align="right">
-                  <Typography fontSize="14px">${product.budget}k</Typography>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-
-    </BaseCard>
-
-  );
+type Vendedor = {
+  idVendedor: number | string;
+  numDocVendedor: string;
+  nombreVendedor: string;
+  Estado?: boolean | number;
 };
 
-export default BasicTable;
+function App() {
+
+/*const baseUrl = "https://7e00-190-107-182-178.ngrok-free.app/Vendedor"; /*COLOCAR API*/
+const baseUrl = "http://5963-190-107-182-178.ngrok-free.app/Vendedor";
+//const baseUrl = "https://localhost:7002/Vendedor";
+const [data, setData] = useState<Vendedor[]>([]);
+const [modalInsertar, setModalInsertar] = useState(false);
+const [modalEditar, setModalEditar] = useState(false); 
+const [gestorSeleccion, setGestorSeleccion] = useState<Vendedor>({
+    idVendedor: '',
+    numDocVendedor: '',
+    nombreVendedor: ''
+});
+
+interface HandleChangeEvent extends React.ChangeEvent<HTMLInputElement> {}
+
+const handleChange = (e: HandleChangeEvent): void => {
+  const { name, value } = e.target;
+  setGestorSeleccion({
+    ...gestorSeleccion,
+    [name]: value
+  });
+  console.log(gestorSeleccion);
+}
+
+const abrirCerrarModalInsertar = () => {
+  setModalInsertar(!modalInsertar);
+};
+
+const abrirCerrarModalEditar = () => {
+  setModalEditar(!modalEditar);
+};
+
+const peticionGet=async()=>{
+  await axios.get(baseUrl)
+    .then(response => {
+      setData(response.data as Vendedor[]); // Asignar los datos obtenidos a la variable de estado
+    }).catch(error => {
+      console.error("Error al obtener los datos:", error);
+    })
+};
+
+/*const peticionPost = async () => {
+  delete gestorSeleccion.idVendedor; // Asegurarse de que no se envíe el ID_Vendedor al crear un nuevo registro
+  //gestorSeleccion.numDocVendedor = String(gestorSeleccion.numDocVendedor); // Asegurarse de que sea un número
+  await axios.post(baseUrl, gestorSeleccion)
+    .then(response => {
+      setData(data.concat(response.data)); // Agregar el nuevo registro a la lista
+      abrirCerrarModalInsertar(); // Cerrar el modal después de insertar
+    }).catch(error => {
+      console.error("Error al insertar el registro:", error);
+    })
+};
+
+const peticionPut = async () => {
+  gestorSeleccion.idVendedor = parseInt(gestorSeleccion.idVendedor); // Asegurarse de que sea un númer
+  await axios.put(baseUrl+"/"+gestorSeleccion.idVendedor, gestorSeleccion)
+  .then(response => {
+    var respuesta = response.data;
+    var dataAuxiliar = data;
+    dataAuxiliar.map(vendedor => {
+      if(vendedor.idVendedor === gestorSeleccion.idVendedor){
+        vendedor.numDocVendedor = respuesta.numDocVendedor;
+        vendedor.nombreVendedor = respuesta.nombreVendedor;
+      }
+    })
+    abrirCerrarModalEditar(); // Cerrar el modal después de editar
+  }).catch(error => {
+    console.error("Error al editar el registro:", error);
+  })
+}*/
+
+interface SeleccionarGestorProps {
+  vendedor: Vendedor;
+  caso: string;
+}
+
+const seleccionarGestor = (vendedor: Vendedor, caso: string): void => {
+  setGestorSeleccion(vendedor);
+  (caso === "Editar") &&
+    abrirCerrarModalEditar();
+};
+
+  useEffect(() => {
+    peticionGet();
+  }, []);
+
+  return (
+    <div className="App">
+      <br />
+      <h1>TABLA VENDEDORES</h1>
+      <button onClick={()=>abrirCerrarModalInsertar()} className='btn btn-success'>Insertar Nuevo Vendedor</button>
+      <br /><br />
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>N° Documento</th>
+            <th>Nombre</th>
+            <th>Activo</th>
+            <th>Acciones</th>            
+          </tr>
+        </thead>
+        <tbody>
+          {data.map(vendedor=>(
+            <tr key={vendedor?.idVendedor ?? `${vendedor.numDocVendedor}-${Math.random()}`}>
+             <td>{vendedor.idVendedor}</td>
+             <td>{vendedor.numDocVendedor}</td>
+             <td>{vendedor.nombreVendedor}</td>
+             <td>{vendedor.Estado ? "0" : "1"}</td>
+             <td>
+                <button className="btn btn-primary" onClick={()=>seleccionarGestor(vendedor, "Editar")}>Editar</button> {" "}
+                <button className="btn btn-danger">Eliminar</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>  
+      </table>
+
+      {/* Modal para insertar un nuevo gestor de base de datos */}
+      <Modal isOpen={modalInsertar}>
+        <ModalHeader>Insertar Gestor de Base de Datos</ModalHeader>
+        <ModalBody>
+          <div className="form-group">
+            {/* <label>ID: </label>
+            <br />
+            <input type="text" className="form-control" name='idVendedor' onChange={handleChange}/>
+            <br /> */}
+            <label>N° Documento: </label>
+            <br />
+            <input type="text" className="form-control" name='numDocVendedor' onChange={handleChange}/>
+            <br />
+            <label>Nombre Vendedor: </label>
+            <br />
+            <input type="text" className="form-control" name='nombreVendedor' onChange={handleChange}/>
+            <br />
+          </div>
+        </ModalBody>
+        <ModalFooter>
+          {/* <button className="btn btn-primary" onClick={()=>peticionPost()}>Insertar</button> */}
+          <button className="btn btn-danger" onClick={()=>abrirCerrarModalInsertar()}>Cancelar</button>
+        </ModalFooter>
+      </Modal>
+
+      {/* Modal para editar un gestor de base de datos */}
+      <Modal isOpen={modalEditar}>
+          <ModalHeader>Editar Gestor de Base de Datos</ModalHeader>
+          <ModalBody>
+            <div className='form-group'>
+              <label>ID: </label>
+              <br />
+              <input type="text" className='form-control' name='idVendedor' readOnly value={gestorSeleccion && gestorSeleccion.idVendedor}/>
+              <br />
+              <label>N° Documento: </label>
+              <br />
+              <input type="text" className='form-control' name="numDocVendedor" onChange={handleChange} value={gestorSeleccion && gestorSeleccion.numDocVendedor}/>
+              <br />
+              <label>Nombre Vendedor: </label>
+              <br />
+              <input type="text" className='form-control' name='nombreVendedor' onChange={handleChange} value={gestorSeleccion && gestorSeleccion.nombreVendedor}/>
+              <br />
+            </div>
+          </ModalBody>
+          <ModalFooter>
+            {/* <button className='btn btn-primary' onClick={()=>peticionPut()}>Editar</button>{" "} */}
+            <button className='btn btn-danger' onClick={()=>abrirCerrarModalEditar()}>Cancelar</button>
+          </ModalFooter>
+      </Modal>
+
+    </div>
+  );
+}
+
+export default App;
